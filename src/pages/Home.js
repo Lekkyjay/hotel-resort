bbd066f808e9a5983972b9ccf195d8596c64ad9c
Lekkyjay <lekkyjayjay@yahoo.com>
2021-04-05 22:11:49 +0200

    Created a dynamic Hero component.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
-    <div>
+    <Hero>
       <h1>Home Page</h1>
-    </div>
+    </Hero>
   )
 }
 
