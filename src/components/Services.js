7ef218acd6404fd4401560f0a85e965f67ba3a2a
Lekkyjay <lekkyjayjay@yahoo.com>
2021-04-05 23:11:24 +0200

    Added Services component to the Home page.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBeer, FaCocktail, FaHiking, FaShuttleVan } from 'react-icons/fa'
+import Title from './Title'
 
 const Services = () => {
+
+  const [services, setServices] = useState([
+    {
+      icon: <FaCocktail />,
+      title: "Free Cocktails",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
+    },
+    {
+      icon: <FaHiking />,
+      title: "Endless Hiking",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
+    },
+    {
+      icon: <FaShuttleVan />,
+      title: "Free Shuttle",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
+    },
+    {
+      icon: <FaBeer />,
+      title: "Strongest Beer",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
+    }
+  ])
+
   return (
-    <div>
-      <h1>Services</h1>
-    </div>
+    <section className="services">
+      <Title title="services" />
+      <div className="services-center">
+        {services.map(item => (
+            <article key={`item-${item.title}`} className="service">
+              <span>{item.icon}</span>
+              <h6>{item.title}</h6>
+              <p>{item.info}</p>
+            </article>
+          )
+        )}
+      </div>
+    </section>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Banner from '../components/Banner'
 import Hero from '../components/Hero'
+import Services from '../components/Services'
 
 const Home = () => {
   return (
-    <Hero>
-      <Banner
-        title="luxurious rooms"
-        subtitle="deluxe rooms starting at $299"
-      >
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <div>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </div>
   )
 }
 
