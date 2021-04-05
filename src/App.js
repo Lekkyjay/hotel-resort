96f2738cb6879cc57a702f253c32ab5e4eabf599
Lekkyjay <lekkyjayjay@yahoo.com>
2021-04-05 21:53:51 +0200

    Navbar implemented.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router';
 import './App.css';
+import Navbar from './components/Navbar';
 import Error from './pages/Error';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
@@ -8,6 +9,7 @@ import SingleRoom from './pages/SingleRoom';
 function App() {
   return (
     <div>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
