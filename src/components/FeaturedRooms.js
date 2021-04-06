30f9367459476e280b5a8c5ee8fb5fb6694889dd
Lekkyjay <lekkyjayjay@yahoo.com>
2021-04-06 10:40:10 +0200

    Context data provision tested.

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { RoomContext } from '../context'
+
+const FeaturedRooms = () => {
+  const { capacity } = useContext(RoomContext)
+  return (
+    <div>
+      <h1>Featured rooms { capacity }</h1>
+    </div>
+  )
+}
+
+export default FeaturedRooms
+
+//cont from 1:45:00
