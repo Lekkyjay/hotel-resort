b8afead4090388e7dad71de48d29f36221176ec8
Lekkyjay <lekkyjayjay@yahoo.com>
2021-04-06 18:14:40 +0200

    Room filter functionality implemented.

--- src/context.js
+++ src/context.js
@@ -57,11 +57,73 @@ export const RoomContextProvider = ({ children }) => {
     const room = tempRooms.find(room => room.slug === slug);
     return room;
   }
-  
 
+  const handleChange = event => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    console.log(target, name, value)
+    setData(data => ({
+      ...data,
+      [name]: value,
+      
+    }))
+  }
+  
+  const filterRooms = () => {
+    let {rooms, type, capacity, price, minSize, maxSize, breakfast, pets } = data;
+
+    let tempRooms = [...rooms]
+
+    capacity = parseInt(capacity);
+    // price = parseInt(price);    
+    
+    // filter by type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+        
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+    }
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
+    //filter by size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+
+    setData(data => ({...data, sortedRooms: tempRooms}));
+  }
+
+  useEffect(() => {
+    filterRooms()
+  }, [
+    data.type,
+    data.capacity,
+    data.price,
+    data.minPrice,
+    data.maxPrice,
+    data.minSize,
+    data.maxSize,
+    data.breakfastalse,
+    data.pets
+  ])
 
   return (
-    <RoomContext.Provider value={{ ...data, getRoom }}>
+    <RoomContext.Provider value={{ ...data, getRoom, handleChange }}>
       { children }
     </RoomContext.Provider>
   )
